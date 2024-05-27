7612977a6edd92ae26623ef71e4f2a74996f3ce8
a2c_hyogo <hyogo.atsushi.cs@daiichisankyo.co.jp>
2024-05-27 18:55:52 +0900

    revactor command.lua

--- lua/mdheadnums/command.lua
+++ lua/mdheadnums/command.lua
@@ -1,6 +1,8 @@
+local mdheadnums = require("mdheadnums")
+
 local commands = {
-	add = require("mdheadnums").add_heading_numbers,
-	remove = require("mdheadnums").remove_heading_numbers,
+	add = mdheadnums.add_heading_numbers,
+	remove = mdheadnums.remove_heading_numbers,
 }
 
 local M = {}
@@ -9,12 +11,11 @@ function M.setup()
 	vim.api.nvim_create_user_command("MDHeading", function(args)
 		if commands[args.args] then
 			commands[args.args]()
+		else
+			print("Invalid command. Use 'add' or 'remove'.")
 		end
 	end, {
 		nargs = 1,
-		complete = function()
-			return { "add", "remove" }
-		end,
 	})
 end
 
